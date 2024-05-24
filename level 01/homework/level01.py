c4bb062b787c4bb428d2347e0dd68aca53cc0894
GiorgiPertaia <pertaia.giosh@gmail.com>
2024-05-24 19:47:58 +0400

    homework

--- level 01/homework/level01.py
+++ level 01/homework/level01.py
@@ -1,1 +1,16 @@
-print("ძალიან მომეწონა GOA აკადემია ძალიან მეგობრული გარემოა ახსნა შესანიშნავი და დარწმუნებული ვარ GOA_ს დახმარებით მივახწევ ჩემს დასახულ მიზნებს Web development_ში ")
+print("ძალიან მომეწონა GOA აკადემია ძალიან მეგობრული გარემოა ახსნა შესანიშნავი და დარწმუნებული ვარ GOA_ს დახმარებით მივახწევ ჩემს დასახულ მიზნებს Web development_ში")
+
+
+
+
+
+name = "giorgi"
+surname = "pertaia"
+age = 17
+height = 172.5
+print(name)
+print(surname)
+print(age)
+print(height)
+
+print(name +" " + surname)
