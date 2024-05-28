2770e9caf6451e02637fa1b9a3d3cd24d07c62fe
GiorgiPertaia <pertaia.giosh@gmail.com>
2024-05-28 23:11:49 +0400

    homework

---
+++ level 03/homework/level03.py
@@ -0,0 +1,53 @@
+name = "giorgi"
+surname = "pertaia"
+age = 17
+height = 172.5
+country = "georgia"
+hobby = "basketball"
+
+print(name + surname + age + height + country + hobby)
+
+
+print(1+1)
+print(7+7)
+print(24+27)
+print(50+27)
+print(70+35)
+print(50+75)
+print(90+34)
+print(40+26)
+print(76+56)
+print(600+356)
+
+print(50-23)
+print(70-27)
+print(9-3)
+print(654-323)
+print(34-21)
+print(34-11)
+print(23-2)
+print(700-200)
+print(800-256)
+print(250-249)
+
+print(5*5)
+print(3*3)
+print(5*8)
+print(7*7)
+print(9*9)
+print(7*3)
+print(8*8)
+print(8*9)
+print(4*4)
+print(2*6)
+
+print(40//2)
+print(21//3)
+print(6//3)
+print(36//6)
+print(25//5)
+print(81//9)
+print(49//7)
+print(30//10)
+print(20//5)
+print(60//3)
